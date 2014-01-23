2df394dc08fc922d16a0da351a01849a1ccafd31
Oguzhan Uysal <development@oguzhanuysal.eu>
2014-01-23 02:27:31 +0100

    First step to splitting logic into a seperate class

---
+++ HathZipper/HathZipper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HathZipper
+{
+    class HathZipper
+    {
+        public delegate void GalleryUpdateHandler(object sender, ProgressEventArgs e);
+        public event GalleryUpdateHandler OnGalleryUpdateStatus;
+    }
+
+    public class ProgressEventArgs : EventArgs
+    {
+        public string Status { get; private set; }
+
+        public ProgressEventArgs(string status)
+        {
+            Status = status;
+        }
+    }
+}
